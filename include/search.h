1aa367436ae3f4855ba9303180034c4764d44e2c
Wes1986 <W.schnipke2989@student.nu.edu>
2025-10-22 19:25:04 -0400

    Update search.h
    fixed a couple typos

--- include/search.h
+++ include/search.h
@@ -3,10 +3,10 @@
 
 
 // Recursive function to preform searchs on an array
-bool binary_search_recursive(int* values, intkey, int start, int end);
+bool binary_search_recursive(int* values, int key, int start, int end);
 
 
 // Function to perform binary search on a sorted array
 int binary_search(int* values, int key, int size);
 
-#endif //GROUP_TWO_PROJECT_BINARYSEARCH_H
+#endif //GROUP_TWO_PROJECT_SEARCH_H
